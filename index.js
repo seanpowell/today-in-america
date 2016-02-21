27a4ca33e1d7504e6ed0fcada74bb14cfade9424
Vladimir <vsetka@gmail.com>
2016-02-21 22:03:02 +0100

    Clean up
    First working version, credentials changed and removed

--- index.js
+++ index.js
@@ -1,41 +1,149 @@
+#!/bin/env node
 var express = require('express');
 var pmongo  = require('promised-mongo');
-var app     = express();
+var fs      = require('fs');
 
-//var mongoUrl   = process.env.OPENSHIFT_MONGODB_DB_URL || 'localhost:27017';
+var TodayInAmerica = function() {
+    var self = this;
 
-var serverPort = process.env.OPENSHIFT_NODEJS_PORT     || 8080;
-var serverIP   = process.env.OPENSHIFT_NODEJS_IP       || '127.0.0.1';
+    /**
+     *  Set up server IP address and port # using env variables/defaults.
+     */
+    self.setupVariables = function() {
+        //  Set the environment variables we need.
+        self.serverPort   = process.env.OPENSHIFT_NODEJS_PORT     || 8080;
+        self.serverIP     = process.env.OPENSHIFT_NODEJS_IP;
 
-var mongoHost  = process.env.OPENSHIFT_MONGODB_DB_HOST ||  'localhost';
-var mongoPort  = process.env.OPENSHIFT_MONGODB_DB_PORT || 27017;
+        self.mongoHost    = process.env.OPENSHIFT_MONGODB_DB_HOST;
+        self.mongoPort    = process.env.OPENSHIFT_MONGODB_DB_PORT || 27017;
+        self.mongoUser    = process.env.OPENSHIFT_MONGODB_DB_USERNAME;
+        self.mongoPass    = process.env.OPENSHIFT_MONGODB_DB_PASSWORD;
 
-var mongoCS    = 'mongodb://admin:EwKhs6iF5ZXD@' + mongoHost + ':' + mongoPort + '/tia';
+        if (typeof self.mongoHost === "undefined") {
+            self.mongoCS  = 'mongodb://localhost:' + self.mongoPort + '/tia';
+        } else {
+            self.mongoCS  = 'mongodb://' + self.mongoUser + ':' + self.mongoPass + '@' + self.mongoHost + ':' + self.mongoPort + '/tia';
+        }
 
-app.set('port', serverPort);
-app.use(express.static(__dirname + '/public'));
+        if (typeof self.serverIP === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            self.serverIP = "127.0.0.1";
+        };
+    };
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+    /**
+     *  terminator === the termination handler
+     *  Terminate server on receipt of the specified signal.
+     *  @param {string} sig  Signal to terminate on.
+     */
+    self.terminator = function(sig){
+        if (typeof sig === "string") {
+           console.log('%s: Received %s - terminating sample app ...', Date(Date.now()), sig);
+           process.exit(1);
+        }
 
-app.get('/', function(request, response) {
-    esponse.render('pages/index');
-});
+        console.log('%s: Node server stopped.', Date(Date.now()) );
+    };
 
-app.get('/db', function (request, response) {
-	console.log(mongoCS);
+    /**
+     *  Setup termination handlers (for exit and a list of signals).
+     */
+    self.setupTerminationHandlers = function(){
+        //  Process on exit and signals.
+        process.on('exit', function() { self.terminator(); });
 
-	_pmongo = pmongo(mongoCS);
-	_pmongo.collection('users').findOne({ })
-		.then(function(users) {
-			//response.render('pages/db', { results: [{ 'id': 'test1', 'name': 'name1' }, { 'id': 'test2', 'name': 'name2' }] } );
-			response.render('pages/db', { results: users } );
-		})
-		.catch(function(err) {
-			response.render('pages/error', { error: { 'errorCode': '1', 'errorMessage': err.message } } );
-		});
-})
+        // Removed 'SIGPIPE' from the list - bugz 852598.
+        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+        ].forEach(function(element, index, array) {
+            process.on(element, function() { self.terminator(element); });
+        });
+    };
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
+    /*  ================================================================  */
+    /*  App server functions (main app logic here).                       */
+    /*  ================================================================  */
+
+    /**
+     *  Create the routing table entries + handlers for the application.
+     */
+    self.createRoutes = function() {
+        self.routes = { };
+
+        self.routes['/db'] = function(request, response) {
+            console.log(self.mongoCS);
+
+			_pmongo = pmongo(self.mongoCS);
+			_pmongo.collection('users').find({ })
+				.then(function(users) {
+					//response.render('pages/db', { results: [{ 'id': 'test1', 'name': 'name1' }, { 'id': 'test2', 'name': 'name2' }] } );
+					response.render('pages/db', { results: users } );
+				})
+				.catch(function(err) {
+					response.render('pages/error', { error: { 'errorCode': '1', 'errorMessage': err.message } } );
+				});
+        };
+
+        self.routes['/'] = function(request, response) {
+        	response.setHeader('Content-Type', 'text/html');
+		    response.render('pages/index');
+		};
+
+        /*self.routes['/'] = function(req, res) {
+            res.setHeader('Content-Type', 'text/html');
+            console.log('rendering body');
+            res.send('<html><body>adasdas</body></html>');
+        };*/
+    };
+
+    /**
+     *  Initialize the server (express) and create the routes and register
+     *  the handlers.
+     */
+    self.initializeServer = function() {
+        self.createRoutes();
+        self.app = express();//express.createServer();
+        console.log('initializing the server');
+
+        //self.app.set('port', serverPort);
+		self.app.use(express.static(__dirname + '/public'));
+		self.app.set('views', __dirname + '/views');
+		self.app.set('view engine', 'ejs');
+
+        console.log('adding routes');
+        //  Add handlers for the app (from the routes).
+        for (var r in self.routes) {
+            self.app.get(r, self.routes[r]);
+        }
+    };
+
+    /**
+     *  Initializes the sample application.
+     */
+    self.initialize = function() {
+        self.setupVariables();
+        self.setupTerminationHandlers();
+
+        // Create the express server and routes.
+        self.initializeServer();
+    };
+
+    /**
+     *  Start the server (starts up the sample application).
+     */
+    self.start = function() {
+        self.app.listen(self.serverPort, self.serverIP, function() {
+            console.log('%s: Node server started on %s:%d ...', Date(Date.now() ), self.serverIP, self.serverPort);
+        });
+    };
+
+};
+
+/**
+ *  main():  Main code.
+ */
+var tia = new TodayInAmerica();
+tia.initialize();
+tia.start();
